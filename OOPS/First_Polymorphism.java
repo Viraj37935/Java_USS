70bc2c938d9262d9a0ed26da78fb8fad979be0f4
Viraj37935 <virajdimble6@gmail.com>
2025-10-01 16:09:51 +0530

    compile time Polymorphism

---
+++ OOPS/First_Polymorphism.java
@@ -0,0 +1,35 @@
+package Polymorphism;
+
+public class First_Polymorphism {
+
+	public static void main(String[] args) {
+		
+		Aeroplane i = new Aeroplane();
+		
+		i.OnBorad("Indigo");
+		i.A_num="543!@vshv";
+
+		
+		i.Filghts();
+	}
+
+}
+
+class Aeroplane {
+	
+	String A_num;
+	String flight;
+	
+	public void Filghts() {
+		
+		
+		System.out.println("Attention Next Flight "+ flight +  A_num);
+	}
+	
+	public String OnBorad( String name) {
+		
+		this.flight=name;
+		return name;
+		
+	}
+}
