ea655fb9b899d09aa397db16c885fa7a299cb7ee
Viraj37935 <virajdimble6@gmail.com>
2025-10-01 10:53:27 +0530

    AccessModifier

---
+++ Access_Modifier/First.java
@@ -0,0 +1,24 @@
+package Access_Modifier;
+
+public class First {
+
+	public void Display() {
+		
+		System.out.println("Public");
+	}
+
+	protected void Display_2() {
+		
+		System.out.println("Protected");
+	}
+	
+	void Display_3() {
+		
+		System.out.println("Default");
+	}
+	
+	private void Display_4() {
+		
+		System.out.println("Private");
+	}
+}
---
+++ OOPS/Access_m_Check.java
@@ -0,0 +1,15 @@
+package Encapsulation;
+import Access_Modifier.First;
+public class Access_m_Check extends First {
+
+	public static void main(String[] args) {
+		
+		new Access_m_Check().Display_2(); // Protected 
+
+	}
+
+}
+
+class main extends First{
+	
+}
