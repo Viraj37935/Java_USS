cc9db4ed1db2a6a3fdaeafcf7bfc3ccd203e7bc5
Viraj37935 <virajdimble6@gmail.com>
2025-12-23 19:50:08 +0530

    Practice

--- Collection_Framework/List_interface/Array_list_1st.java
+++ Collection_Framework/List_interface/Array_list_1st.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Array_list_1st {
+	
+	/*
+	 * ArrayList - 
+	 * - internally use dynamic array
+	 * - it maintain insertion order
+	 * - useful for random access of element(due to index based) has O(1) constant time complexity
+	 * - not useful for insertion and deletion of element. because internal shifting of indices - O(1) linear time complexity
+	 * - initial cap. is 10
+	 * - allow duplicates and multiple null values
+	 * */
+
 
 	public static void main(String[] args) {
 		
 		List<Integer> obj = new ArrayList<Integer>();
-		
-		
-		/**
-		 * ArrayList=> re-sizeable array.
-		 */
 		
 		
 		obj.add(15);
