d2d6c30244e3146a6aab73973f16d08529c6d2a4
Viraj37935 <virajdimble6@gmail.com>
2025-12-26 20:38:31 +0530

    Iterator Class

---
+++ Collection_Framework/Itorator/First_java.java
@@ -0,0 +1,50 @@
+package itorator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class First_java {
+
+	public static void main(String[] args) {
+		List<Author> authors = new ArrayList<Author>();
+
+		for (int i = 0; i < 15; i++) {
+			authors.add(new Author("A", i));
+		}
+
+		System.out.println(authors);
+
+		Iterator<Author> itr = authors.iterator();
+
+		System.out.println(authors.size());
+		while (itr.hasNext()) {
+			Author author = itr.next();
+			if (author.mobile > 10) {
+				itr.remove();
+			} else {
+				System.out.println("ineligible");
+			}
+
+		}
+
+		System.out.println(authors.size());
+		System.out.println(authors);
+
+	}
+
+}
+
+class Author {
+	String name;
+	int mobile;
+
+	public Author(String name, int mobile) {
+		this.name = name;
+		this.mobile = mobile;
+	}
+
+	public String toString() {
+		return this.mobile + " ";
+	}
+}
---
+++ Collection_Framework/Itorator/Second_2.java
@@ -0,0 +1,29 @@
+package itorator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Second_2 {
+
+	public static void main(String[] args) {
+		List<Integer> list2 = new ArrayList<Integer>();
+
+		for (int i = 0; i < 10; i++) {
+			list2.add(i);
+		}
+
+		ListIterator<Integer> litr = list2.listIterator();
+
+		while (litr.hasNext()) {
+			if (litr.next() == 5) {
+				litr.remove();
+			} else {
+				System.out.println(litr.next());
+			}
+		}
+		System.out.println(list2);
+	}
+
+}
+
