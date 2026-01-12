3e8cb18b830a254ac979d1005a5471aae38c9eb2
Viraj37935 <virajdimble6@gmail.com>
2026-01-12 23:08:43 +0530

    Stream using in Collection

---
+++ Stream_API/Collection_Class/Third.java
@@ -0,0 +1,39 @@
+package collection_class;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Third {
+
+	public static void main(String[] args) {
+	 List<String> list=Arrays.asList("abc","   ","       ","a","bcde");
+		 
+		 String stre="hello";
+		 
+		 
+	 CheckArbitary ch=new CheckArbitary();
+		 
+		 //String=>
+         list.stream().map(stre::concat)
+         .map(String::toUpperCase)
+         .map(String::length)   //Integer
+         .map(ch::square)
+         .forEach(System.out::println);
+	}
+
+	}
+
+class  CheckArbitary
+{
+	static int counter=1;
+	   public   boolean der(String str)
+	   {
+		   System.out.println(++counter);
+		   return  !str.isBlank();
+	   }
+	   
+	   public  Integer square(Integer i)
+	   {
+		   return i*i;
+	   }
+}
